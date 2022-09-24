960d9aab1caadf06e97a4acd50f2846b066f5277
JuliKrue <35929281+JuliKrue@users.noreply.github.com>
2022-09-24 11:08:30 +0200

    feat(TourOfHeroes): Added Dashboard component, Added component styles,Added Hero Service, Added hero route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
 
 //Route hat 2 Eigenschaften: Path und Component. Path = String gleich der URL im Browser (z.B. .../.../heroes), Component = Aufgerufene Komponente der Route
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent }
 ];
 
